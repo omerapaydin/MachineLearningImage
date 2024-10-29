94192783226438002bfbb37f1cb09d8f38797372
Ömer Apaydın <omerapaydin90@gmail.com>
2024-10-29 16:55:59 +0300

    Initial Commit

---
+++ MachineLearningImageTests/MachineLearningImageTests.swift
@@ -0,0 +1,17 @@
+//
+//  MachineLearningImageTests.swift
+//  MachineLearningImageTests
+//
+//  Created by Ömer on 29.10.2024.
+//
+
+import Testing
+@testable import MachineLearningImage
+
+struct MachineLearningImageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
